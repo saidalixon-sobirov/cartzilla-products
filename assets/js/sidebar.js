e594f1280bee50142550558aa73cb9dee47ad094
saidalixon-sobirov <saidalixon_sobirov@mail.ru>
2022-11-22 15:51:41 +0500

    23-dars

---
+++ assets/js/sidebar.js
@@ -0,0 +1,11 @@
+// "use strict";
+
+// const current = document.querySelectorAll(".current");
+
+// current.forEach((item) => {
+//   item.addEventListener("click", (e) => {
+//     e.target.toggle("show")
+//   });
+// });
+
+
